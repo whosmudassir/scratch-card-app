7627a022f7a2fd0fc5ec1d8450e792a03c322a77
deadsoulartist <deadsoulartist@gmail.com>
2025-02-28 11:00:26 +0530

    fix: removed dark theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,4 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -10,13 +6,11 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import { TransactionProvider } from "../context/TransactionContext";
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -32,14 +26,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <TransactionProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(payment)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </TransactionProvider>
-
       <StatusBar style="auto" />
     </ThemeProvider>
   );
